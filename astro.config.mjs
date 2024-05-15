3d00d6b7a8a2592cb816a543db1f2caa339f5bb7
Noah <hi@noahflk.com>
2024-05-15 13:14:08 -0600

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+  },
+});
